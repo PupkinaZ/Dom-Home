67e669e222a851997333b33bce0d18102962ba4e
PupkinaZ <136488269+PupkinaZ@users.noreply.github.com>
2023-06-13 19:48:21 +0300

    Add files via upload

---
+++ js/agencies.js
@@ -0,0 +1,63 @@
+//List of agencies
+var agencies = [
+	{
+		"id": 0,
+		"title": "В Дмитрове",
+		"latitude":56.3447036504141,
+		"longitude":37.531100329868174,
+		"image":"http://placehold.it/700x603",
+		"description":"Дмитров, ул. Профессиональная <br> Телефон: (123) 546-7890",
+		"link":"agency-detail.html",
+		"map_marker_icon":"images/markers/coral-marker-cozy.png"
+	},
+	{
+		"id": 1,
+		"title": "В Москве",
+		"latitude":55.75885090838364,
+		"longitude":37.66963027642131,
+		"image":"http://placehold.it/700x603",
+		"description":"Москва, ул. Кангина, д. 23 <br> Телефон: (123) 546-7890",
+		"link":"agency-detail.html",
+		"map_marker_icon":"images/markers/coral-marker-cozy.png"
+	},
+	{
+		"id": 2,
+		"title": "В Талдоме",
+		"latitude":56.73235309312506,
+		"longitude":37.524517558848494,
+		"image":"http://placehold.it/700x603",
+		"description":"Рязань, ул. Вавская, д. 56 <br> Телефон: (123) 546-7890",
+		"link":"agency-detail.html",
+		"map_marker_icon":"images/markers/coral-marker-cozy.png"
+	},
+	{
+		"id": 3,
+		"title": "В Кимрах",
+		"latitude":56.86188300127213,
+		"longitude":37.35434936461427,
+		"image":"http://placehold.it/700x603",
+		"description":"Кимры, ул. Кайская, д. 13 <br> Телефон: (123) 546-7890",
+		"link":"agency-detail.html",
+		"map_marker_icon":"images/markers/coral-marker-cozy.png"
+	},
+	{
+		"id": 4,
+		"title": "В Белгороде",
+		"latitude":50.61515365813818,
+		"longitude":36.558758963694,
+		"image":"http://placehold.it/700x603",
+		"description":"Белгород, ул. Внукова, д. 6 <br> Телефон: (123) 546-7890",
+		"link":"agency-detail.html",
+		"map_marker_icon":"images/markers/coral-marker-cozy.png"
+	},
+	{
+		"id": 5,
+		"title": "В Рязани",
+		"latitude":54.60662773480811,
+		"longitude":39.714874945544715,
+		"image":"http://placehold.it/700x603",
+		"description":"Рязань, ул. Ленина, д. 2 <br> Телефон: (123) 546-7890",
+		"link":"agency-detail.html",
+		"map_marker_icon":"images/markers/coral-marker-cozy.png"
+	}
+];
